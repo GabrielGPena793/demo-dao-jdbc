6ef3c0039d2359e47595ddd16ac50d360c1bb918
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-02-11 01:19:59 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entitites.Department;
 import model.entitites.Seller;
 
@@ -9,7 +12,11 @@ public class Program {
 
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
+
         Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
 
     }
